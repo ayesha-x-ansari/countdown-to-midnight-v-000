20f4d39a96e946657731de1d14232e3c6bc9cf75
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-04 05:32:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,10 +2,10 @@
 number = 10
 def countdown(number)
   while number > 0
-   puts "#{number} SECOND(S)!"
-   number -= 1
-   if nmuber = 0
-      "HAPPY NEW YEAR!"
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    if nmuber == 0
+      return  "HAPPY NEW YEAR!"
    end
  end
 end
