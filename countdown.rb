2330faaf6da8b251bae190c22b381ec7dc849c5b
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-04 05:38:37 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -15,7 +15,7 @@ countdown(number)
 number = 10
 def countdown_with_sleep(number)
   while number > 0
-    sleep 20
+    sleep 2
     puts "#{number} SECOND(S)!"
     number -= 1
     if number == 0
