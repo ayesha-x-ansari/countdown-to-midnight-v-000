6261f46de328281049ec750d4e61bcfa7e1f3d54
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-04 05:35:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -14,4 +14,12 @@ countdown(number)
 
 
 def countdown_with_sleep
+  while number > 0
+    sleep 5
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    if number == 0
+      return  "HAPPY NEW YEAR!"
+   end
+ end
 end
